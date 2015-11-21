21de438cad5fda15a4edc11ba4358b47f620c581
Freshield <zxdsw199182@gmail.com>
2015-11-21 17:15:18 -0500

    finished exercise5

--- exercise5/src/exercise5/DEMO.java
+++ exercise5/src/exercise5/DEMO.java
@@ -8,6 +8,32 @@ public class DEMO {
 		
 		System.out.println(o.toString());
 		System.out.println("interesting");
+		
+		int a = -3;
+		int b = +a;
+		int c = -a;
+		
+		a = 3;
+		b = ++a;
+		c = a++;
+		b = --a;
+		c = a--;
+		
+		a = 2;
+		b = a + 2;
+		c = b - 2;
+		a = c*2;
+		b = a/2;
+		c = b%2;
+		
+		a = 11;
+		b = 22;
+		
+		boolean d = (a >= b);
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(d);
 
 	}
 
