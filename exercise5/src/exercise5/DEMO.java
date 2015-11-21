95bae1918c7315bf7a659650f1d5f5cc320185f7
Freshield <zxdsw199182@gmail.com>
2015-11-21 17:06:35 -0500

    rebegin

--- exercise5/src/exercise5/DEMO.java
+++ exercise5/src/exercise5/DEMO.java
@@ -4,6 +4,10 @@ public class DEMO {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Object o = new Object();
+		
+		System.out.println(o.toString());
+		System.out.println("interesting");
 
 	}
 
