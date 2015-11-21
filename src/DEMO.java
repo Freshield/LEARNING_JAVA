24d86aae6f80446364d11e826d570525175ddfd0
Freshield <zxdsw199182@gmail.com>
2015-11-21 15:13:08 -0500

    try the new branch of wrong name

--- src/DEMO.java
+++ src/DEMO.java
@@ -3,27 +3,10 @@ public class DEMO {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Object o = new Object();
-		
-		System.out.println();
-		System.out.println(o.toString());
-		System.out.println("FINISHED");
-		System.out.println();
-		
-		char a = 'ÓÚ';
-		int b = a;
-		char c = 20110;
-		char d = '¹Ú';
-		int e = d;
-		char f = 27915;
-		char g = '\t';
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
-		System.out.println(d);
-		System.out.println(e);
-		System.out.println(f);
-		System.out.println(g);
+		String a = "22";
+		System.out.println(a.getClass());
+		Integer String = 22;
+		System.out.println(String.getClass());
 
 	}
 
