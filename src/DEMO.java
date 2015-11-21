b6eb6a4648e1af73d7a5d226ce9edcb7e82413f9
Freshield <zxdsw199182@gmail.com>
2015-11-21 15:16:08 -0500

    using 中文写入

--- src/DEMO.java
+++ src/DEMO.java
@@ -1,8 +1,12 @@
 
 public class DEMO {
+	
+	int 中文属性 = 22;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		DEMO 中文变数 = new DEMO();
+		System.out.println(中文变数.中文属性);
 		String a = "22";
 		System.out.println(a.getClass());
 		Integer String = 22;
