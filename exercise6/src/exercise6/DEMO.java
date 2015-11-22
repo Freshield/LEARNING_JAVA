9df8870fbdcdf57716e07b2e997519e866134ef5
Freshield <zxdsw199182@gmail.com>
2015-11-21 22:05:29 -0500

    finished exercise6

--- exercise6/src/exercise6/DEMO.java
+++ exercise6/src/exercise6/DEMO.java
@@ -4,6 +4,41 @@ public class DEMO {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		if(3>5)
+		{
+			System.out.println("error");
+		}
+		else if(4>5)
+		{
+			System.out.println("error2");
+		}
+		else if(5>5)
+		{
+			System.out.println("error3");
+		}
+		else if(6>5)
+		{
+			System.out.println("correct");
+		}
+		else
+		{
+			System.out.println("nothing");
+		}
+		
+		switch(6)
+		{
+		case 3:
+			System.out.println("3");
+			break;
+		case 6:
+			System.out.println("6");
+			break;
+		default:
+			System.out.println("nothing");
+		}
+		
+		
 
 	}
 
