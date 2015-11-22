63ac3d64ca828e003d5e17dc6fe72627860a0191
Freshield <zxdsw199182@gmail.com>
2015-11-22 15:16:22 -0500

    finished loop

---
+++ exercise7/src/exercise7/DEMO.java
@@ -0,0 +1,21 @@
+package exercise7;
+
+public class DEMO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;
+		while(i>0)
+		{
+
+			System.out.println(i);
+			i --;
+		}
+		for(int j = 10; j > 0;j--)
+		{
+			System.out.println(j);
+		}
+
+	}
+
+}
