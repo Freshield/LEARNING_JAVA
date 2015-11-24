ecb9158b009926e7e2ce4bc3a3e2d7c417071ea2
Freshield <zxdsw199182@gmail.com>
2015-11-24 14:33:36 -0500

    try api import

---
+++ API/src/API.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class API {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Date d = new Date();
+		
+		System.out.println(d);
+
+	}
+
+}
