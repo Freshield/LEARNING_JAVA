c40584fa448f68fdd70cb6e597ba5b150485b2a4
Freshield <zxdsw199182@gmail.com>
2015-11-24 14:39:15 -0500

    try the String replace

--- API/src/API.java
+++ API/src/API.java
@@ -9,6 +9,34 @@ public class API {
 		Date d = new Date();
 		
 		System.out.println(d);
+		
+		//try string
+		
+		String s = "There is no spoon";
+		
+		System.out.println(s);
+		
+		String s1 = s.replace("a", "e");
+		
+		System.out.println(s1);
+		
+		String s2 = s1.replace("e", "i");
+		
+		System.out.println(s2);
+		
+		String s3 = s2.replace("i", "o");
+		
+		System.out.println(s3);
+
+		String s4 = s3.replace("o", "u");
+		
+		System.out.println(s4);
+
+		String s5 = s4.replace("u", "a");
+		
+		System.out.println(s5);
+		
+		
 
 	}
 
