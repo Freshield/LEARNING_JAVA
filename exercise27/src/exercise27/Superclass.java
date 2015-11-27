befd05ed161b319ca467be9ae8a3605869f94eaf
Freshield <zxdsw199182@gmail.com>
2015-11-26 19:05:50 -0500

    subclass init

--- exercise27/src/exercise27/Superclass.java
+++ exercise27/src/exercise27/Superclass.java
@@ -3,9 +3,27 @@ package exercise27;
 public class Superclass {
 	
 	public int superfield;
+	
+	public Superclass()
+	{
+		System.out.println("Superclass");
+	}
+	
+	public void supermethod()
+	{
+		System.out.println("supermethod");
+	}
+	
+	class Subclass extends Superclass
+	{
+		public int subfield;
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
 
